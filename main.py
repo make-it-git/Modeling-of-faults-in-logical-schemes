e082ed522995314fc9cfcffe02bea1fe143c3f49
make.it.git <make.it.git@gmail.com>
2012-10-21 01:49:07 +0400

    Basic modelling

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class BaseComponent:
             raise ValueError("Number of input ports should be positive integer")
         self.__input_ports = [None for i in range(number_of_input_ports)]
         self.__function = function
+        self.__input_lines = [None for i in range(number_of_input_ports)]
+        self.__output_line = None
 
     def set_input(self, i, value):
         if i >= 0 and value in [0, 1]:
@@ -19,7 +21,12 @@ class BaseComponent:
     def out(self):
         '''Perform function and write result to output port'''
         if self.ready():
-            return self.__function(self.__input_ports)
+            value = self.__function(self.__input_ports)
+            if self.__output_line is None:
+                raise Exception("Line is not attached")
+            else:
+                self.__output_line.set_input(value)
+                return value
         else:
             raise Exception("Function not ready to be calculated. Not all inputs are set")
 
@@ -32,7 +39,53 @@ class BaseComponent:
     def clear(self):
         self.__input_ports = [None for i in range(len(self.__input_ports))]
 
+    def attach_output_line(self, line):
+        '''One output port. One line'''
+        self.__output_line = line
 
+    def attach_input_line(self, i, line):
+        self.__input_lines[i] = line
+
+
+class BaseLine:
+
+    def __init__(self):
+        self.__input = None
+        self.__output = None
+        self.__output_lines = []
+
+    def set_input(self, value):
+        if value in [0, 1]:
+            if self.__output is None:#no failures
+                self.__input = value
+            else:
+                self.__input = self.__output
+            for line in self.__output_lines: #propagate signal to all lines which are connected to this line(self)
+                line.set_input(self.__input)
+        else:
+            raise ValueError("value should be 0 or 1")
+
+    def set_output(self, value):
+        '''If scheme works correctly (without failures)
+        then set_output is not called'''
+        if value in [0, 1]:
+            self.__output = value
+        else:
+            raise ValueError("value should be 0 or 1")
+
+    def get_value(self):
+        if self.__output is not None:#failure
+            return self.__output
+        else:
+            return self.__input
+
+    def clear(self):
+        self.__input = None
+        self.__output = None
+
+    def attachOutputLine(self, line):
+        '''should call line.set_input every time something changes in self'''
+        self.__output_lines.append(line)
 
 if __name__ == '__main__':
     pass
--- test.py
+++ test.py
@@ -5,11 +5,12 @@ import main
 import unittest
 def function_and(inputs):
     return inputs[0] and inputs[1]
+def function_or(inputs):
+    return inputs[0] or inputs[1]
+def function_in(inputs):
+    return inputs[0]
 
 class TestBaseComponent(unittest.TestCase):
-
-    def setUp(self):
-        self.inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
     def test_exception(self):
         base_component = main.BaseComponent(2, function_and)
@@ -30,6 +31,8 @@ class TestBaseComponent(unittest.TestCase):
 
     def test_out(self):
         base_component = main.BaseComponent(2, function_and)
+        base_line = main.BaseLine()
+        base_component.attach_output_line(base_line)
         base_component.set_input(0, 0)
         base_component.set_input(1, 0)
         self.assertEqual(0, base_component.out())
@@ -50,6 +53,112 @@ class TestBaseComponent(unittest.TestCase):
         self.assertTrue(base_component.ready())
         base_component.clear()
         self.assertFalse(base_component.ready())
+
+class TestBaseLine(unittest.TestCase):
+
+    def test_set_input(self):
+       base_line = main.BaseLine()
+       self.assertRaises(ValueError, base_line.set_input,-1)
+       self.assertRaises(ValueError, base_line.set_input, 2)
+       base_line.set_input(1)
+       base_line.set_input(0)
+
+    def test_set_output(self):
+       base_line = main.BaseLine()
+       self.assertRaises(ValueError, base_line.set_output, -1)
+       self.assertRaises(ValueError, base_line.set_output, 2)
+       base_line.set_output(1)
+       base_line.set_output(0)
+
+    def test_clear(self):
+       base_line = main.BaseLine()
+       self.assertIsNone(base_line.get_value())
+       base_line.set_input(1)
+       self.assertEqual(1, base_line.get_value())
+       base_line.clear()
+       self.assertIsNone(base_line.get_value())
+       base_line.set_output(1)
+       self.assertEqual(1, base_line.get_value())
+
+class TestBaseConnection(unittest.TestCase):
+
+    def test_base_connection(self):
+        #inputs
+        comp_in1 = main.BaseComponent(1, function_in)
+        comp_in2 = main.BaseComponent(1, function_in)
+        #and
+        comp_and1 = main.BaseComponent(2, function_and)
+        #lines
+        line_in1_to_and1 = main.BaseLine()
+        line_in2_to_and1 = main.BaseLine()
+        line_out = main.BaseLine()
+
+        comp_in1.attach_output_line(line_in1_to_and1)
+        comp_in2.attach_output_line(line_in2_to_and1)
+        comp_and1.attach_input_line(0, line_in1_to_and1)
+        comp_and1.attach_input_line(1, line_in1_to_and1)
+        comp_and1.attach_output_line(line_out)
+
+        comp_in1.set_input(0, 0)
+        comp_in1.out()
+        comp_in2.set_input(0, 0)
+        comp_in2.out()
+        comp_and1.set_input(0, line_in1_to_and1.get_value())
+        comp_and1.set_input(1, line_in2_to_and1.get_value())
+        comp_and1.out()
+        self.assertEqual(0, line_out.get_value())
+
+        comp_in1.set_input(0, 1)
+        comp_in1.out()
+        comp_in2.set_input(0, 1)
+        comp_in2.out()
+        comp_and1.set_input(0, line_in1_to_and1.get_value())
+        comp_and1.set_input(1, line_in2_to_and1.get_value())
+        comp_and1.out()
+        self.assertEqual(1, line_out.get_value())
+
+    def test_2components(self):
+        '''3 inputs, 1 output
+        in1 and in2 or in3'''
+        #components
+        c_in1 = main.BaseComponent(1, function_in)
+        c_in2 = main.BaseComponent(1, function_in)
+        c_in3 = main.BaseComponent(1, function_in)
+        c_and = main.BaseComponent(2, function_and)
+        c_or = main.BaseComponent(2, function_or)
+        #lines (1 and 2 connects to component 'and')
+        #3 connects to component 'or'
+        #4 links output of 'and' and input of 'or'
+        l_in1 = main.BaseLine()
+        l_in2 = main.BaseLine()
+        l_in3 = main.BaseLine()
+        l_in4 = main.BaseLine()
+        l_out = main.BaseLine()
+
+        c_in1.attach_output_line(l_in1)
+        c_in2.attach_output_line(l_in2)
+        c_and.attach_input_line(0, l_in1)
+        c_and.attach_input_line(1, l_in2)
+        c_and.attach_output_line(l_in4)
+
+        c_in3.attach_output_line(l_in3)
+        c_or.attach_input_line(0, l_in4)
+        c_or.attach_input_line(1, l_in3)
+        c_or.attach_output_line(l_out)
+
+        c_in2.set_input(0, 1)
+        c_in1.set_input(0, 1)
+        c_in3.set_input(0, 0)
+        c_in1.out()
+        c_in2.out()
+        c_in3.out()
+        c_and.set_input(0, l_in1.get_value())
+        c_and.set_input(1, l_in2.get_value())
+        c_and.out()
+        c_or.set_input(0, l_in4.get_value())
+        c_or.set_input(1, l_in3.get_value())
+        c_or.out()
+        self.assertEqual(1, l_out.get_value())
 
 
 
